9036996c79465dc4884144670f54eaa711cdf159
Felipe Padilha <62897335+padilhafe@users.noreply.github.com>
2024-11-28 19:13:10 -0300

    support for table formatting

--- githubctl/utils.py
+++ githubctl/utils.py
@@ -3,6 +3,10 @@ from csv import DictWriter
 from sys import stdout
 from typing import List
 from options import OutputOption
+from rich import print_json
+from rich.table import Table
+from rich.console import Console
+import json
 
 def print_beauty(list_of_dict: List[dict], output_format: OutputOption):
     if output_format == OutputOption.csv:
@@ -10,3 +14,22 @@ def print_beauty(list_of_dict: List[dict], output_format: OutputOption):
         writer = DictWriter(stdout, fieldnames=field_names)
         writer.writeheader()
         writer.writerows(list_of_dict)
+
+    elif output_format == OutputOption.table:
+        table = Table()
+        headers = list_of_dict[0].keys()
+        table.add_column("")
+
+        for header in headers:
+            table.add_column(str(header))
+
+        for repo in list_of_dict:
+            table.add_row(*[str((list_of_dict.index(repo) + 1))] + [str(row) for row in repo.values()])
+
+        console = Console()
+        console.print(table)
+
+
+
+    else:
+        print_json(json.dumps(list_of_dict))
